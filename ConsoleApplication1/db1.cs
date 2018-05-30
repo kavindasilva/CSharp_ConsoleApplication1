394d25a815e4bc192d7b32387c2f89551f15ea47
kavindasilva <kss.kavinda@gmail.com>
2018-05-30 11:21:00 +0530

    MS SQL server database connection
    Not completed yet

---
+++ ConsoleApplication1/db1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/**
+ * This file contains database operations on a 'MS SQL server DB file' (.sdf)
+ * MyDatabase#1.sdf
+ * 
+ */
+
+namespace ConsoleApplication1
+{
+    class db1
+    {
+
+    }
+}
